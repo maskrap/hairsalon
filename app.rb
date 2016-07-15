506e12fcc277999024eaaf5d390debecb86027a3
Sam Park <sampark18@gmail.com>
2016-07-15 10:45:04 -0700

    Add front end for adding stylist form

--- app.rb
+++ app.rb
@@ -5,3 +5,17 @@ require('./lib/client')
 require('./lib/stylist')
 require('pg')
 require('pry')
+
+DB = PG.connect({:dbname => 'hair_salon_test'})
+
+get('/') do
+  @stylists = Stylist.all()
+  # @clients = Client.all()
+  erb(:index)
+end
+
+get('/stylists') do
+  @stylists = Stylist.all()
+  # @clients = Client.all()
+  erb(:index)
+end
